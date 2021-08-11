6922e9ab3f808c4f6c262b9f902b37a0edc019db
Кирилл <47419037+ForDeveloperMan@users.noreply.github.com>
2021-08-11 23:20:58 +0300

    grtert

--- app/js/main.js
+++ app/js/main.js
@@ -61,10 +61,16 @@ if( $(window).width() > 600){
   let dropFix;
 
   $('.links-menu__dropdownWrap').on('mouseenter', function(){
+    if ( $('.top-menu').hasClass('top-menu_fixed') && $(window).width() <= 1023 ) {
+      return false;
+    }
     clearTimeout(dropFix);
     $(this).find('.links-menu__dropdown').fadeIn(400);
   });
   $('.links-menu__dropdownWrap').on('mouseleave', function(){
+    if ( $('.top-menu').hasClass('top-menu_fixed') && $(window).width() <= 1023 ) {
+      return false;
+    }
     clearTimeout(dropFix);
     dropFix = setTimeout(drop, 50, $(this));
     function drop(t){
@@ -126,6 +132,7 @@ $('.block-reviews').owlCarousel({
   nav: true,
   navText: ['<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><circle r="23.5" transform="matrix(-1 0 0 1 24 24)" stroke="#B39013"/><path d="M28.7992 33.6004L19.1992 24.0004L28.7992 14.4004" stroke="#B39013"/></svg>', '<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="24" cy="24" r="23.5" stroke="#B39013"/><path d="M19.2008 33.6004L28.8008 24.0004L19.2008 14.4004" stroke="#B39013"/></svg>'],
   dots: true,
+  smartSpeed: 700,
   responsive: {
     0: {
       margin: 50,
@@ -231,6 +238,11 @@ if ( $(window).width() <= 600 ) {
     dots: true,
   });
 }
+
+
+$('.links-menu__link_title').each(function(){
+  $(this).append('<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12.5" cy="12.5" r="12" stroke="#B39013"/><line class="line-1" x1="12.5" y1="6.5" x2="12.5" y2="18.5" stroke="#B39013" stroke-linecap="round"/><line x1="18.5" y1="12.5" x2="6.5" y2="12.5" stroke="#B39013" stroke-linecap="round"/></svg>');
+});
 
 $('.links-menu__link_title').on('click', function(){
   var wrap = $(this).closest('.links-menu__el');
